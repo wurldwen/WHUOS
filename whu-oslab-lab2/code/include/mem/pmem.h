b430988e29ab42314c3856065551fdd6504e77b9
wurld <1287356875@qq.com>
2025-09-28 18:40:56 +0800

    添加pmem.h,vmem.h,pmem.c

--- whu-oslab-lab2/code/include/mem/pmem.h
+++ whu-oslab-lab2/code/include/mem/pmem.h
@@ -12,4 +12,9 @@ void  pmem_init(void);
 void* pmem_alloc(bool in_kernel);
 void  pmem_free(uint64 page, bool in_kernel);
 
+// 内核占用的物理页数（可通过在编译时用 -DKERNEL_PAGES=N 覆盖）
+#ifndef KERNEL_PAGES
+#define KERNEL_PAGES 6
+#endif
+
 #endif
--- whu-oslab-lab2/code/include/mem/vmem.h
+++ whu-oslab-lab2/code/include/mem/vmem.h
@@ -29,42 +29,43 @@
     PPN区域 : 存放物理页号
 */
 
+// riscv.h中已经有了
 // 页表项
-typedef uint64 pte_t;
+// typedef uint64 pte_t;
 
-// 顶级页表
-typedef uint64* pgtbl_t;
+// // 顶级页表
+// typedef uint64* pgtbl_t;
 
-// satp寄存器相关
-#define SATP_SV39 (8L << 60)  // MODE = SV39
-#define MAKE_SATP(pagetable) (SATP_SV39 | (((uint64)pagetable) >> 12)) // 设置MODE和PPN字段
+// // satp寄存器相关
+// #define SATP_SV39 (8L << 60)  // MODE = SV39
+// #define MAKE_SATP(pagetable) (SATP_SV39 | (((uint64)pagetable) >> 12)) // 设置MODE和PPN字段
 
-// 获取虚拟地址中的虚拟页(VPN)信息 占9bit
-#define VA_SHIFT(level)         (12 + 9 * (level))
-#define VA_TO_VPN(va,level)     ((((uint64)(va)) >> VA_SHIFT(level)) & 0x1FF)
+// // 获取虚拟地址中的虚拟页(VPN)信息 占9bit
+// #define VA_SHIFT(level)         (12 + 9 * (level))
+// #define VA_TO_VPN(va,level)     ((((uint64)(va)) >> VA_SHIFT(level)) & 0x1FF)
 
-// PA和PTE之间的转换
-#define PA_TO_PTE(pa) ((((uint64)(pa)) >> 12) << 10)
-#define PTE_TO_PA(pte) (((pte) >> 10) << 12)
+// // PA和PTE之间的转换
+// #define PA_TO_PTE(pa) ((((uint64)(pa)) >> 12) << 10)
+// #define PTE_TO_PA(pte) (((pte) >> 10) << 12)
 
-// 页面权限控制 
-#define PTE_V (1 << 0) // valid
-#define PTE_R (1 << 1) // read
-#define PTE_W (1 << 2) // write
-#define PTE_X (1 << 3) // execute
-#define PTE_U (1 << 4) // user
-#define PTE_G (1 << 5) // global
-#define PTE_A (1 << 6) // accessed
-#define PTE_D (1 << 7) // dirty
+// // 页面权限控制 
+// #define PTE_V (1 << 0) // valid
+// #define PTE_R (1 << 1) // read
+// #define PTE_W (1 << 2) // write
+// #define PTE_X (1 << 3) // execute
+// #define PTE_U (1 << 4) // user
+// #define PTE_G (1 << 5) // global
+// #define PTE_A (1 << 6) // accessed
+// #define PTE_D (1 << 7) // dirty
 
-// 检查一个PTE是否属于pgtbl
-#define PTE_CHECK(pte) (((pte) & (PTE_R | PTE_W | PTE_X)) == 0)
+// // 检查一个PTE是否属于pgtbl
+// #define PTE_CHECK(pte) (((pte) & (PTE_R | PTE_W | PTE_X)) == 0)
 
-// 获取低10bit的flag信息
-#define PTE_FLAGS(pte) ((pte) & 0x3FF)
+// // 获取低10bit的flag信息
+// #define PTE_FLAGS(pte) ((pte) & 0x3FF)
 
-// 定义一个相当大的VA, 规定所有VA不得大于它
-#define VA_MAX (1ul << 38)
+// // 定义一个相当大的VA, 规定所有VA不得大于它
+// #define VA_MAX (1ul << 38)
 
 void   vm_print(pgtbl_t pgtbl);
 pte_t* vm_getpte(pgtbl_t pgtbl, uint64 va, bool alloc);
