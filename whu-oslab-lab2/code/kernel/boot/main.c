261a3f413e477897cdb64f955dbdf6af8d43ddb6
wurld <1287356875@qq.com>
2025-09-28 21:23:56 +0800

    添加测试代码，准备调试

--- whu-oslab-lab2/code/kernel/boot/main.c
+++ whu-oslab-lab2/code/kernel/boot/main.c
@@ -1,23 +1,79 @@
-#include "riscv.h"
-#include "lib/print.h"
-
 volatile static int started = 0;
+
+volatile static int over_1 = 0, over_2 = 0;
+
+static int* mem[1024];
 
 int main()
 {
     int cpuid = r_tp();
+
     if(cpuid == 0) {
+
         print_init();
-        printf("cpu %d is booting!\n", cpuid);        
+        pmem_init();
+
+        printf("cpu %d is booting!\n", cpuid);
         __sync_synchronize();
         started = 1;
+
+        for(int i = 0; i < 512; i++) {
+            mem[i] = pmem_alloc(true);
+            memset(mem[i], 1, PGSIZE);
+            printf("mem = %p, data = %d\n", mem[i], mem[i][0]);
+        }
+        printf("cpu %d alloc over\n", cpuid);
+        over_1 = 1;
+        
+        while(over_1 == 0 || over_2 == 0);
+        
+        for(int i = 0; i < 512; i++)
+            pmem_free((uint64)mem[i], true);
+        printf("cpu %d free over\n", cpuid);
+
     } else {
+
         while(started == 0);
         __sync_synchronize();
         printf("cpu %d is booting!\n", cpuid);
-    }   
+        
+        for(int i = 512; i < 1024; i++) {
+            mem[i] = pmem_alloc(true);
+            memset(mem[i], 1, PGSIZE);
+            printf("mem = %p, data = %d\n", mem[i], mem[i][0]);
+        }
+        printf("cpu %d alloc over\n", cpuid);
+        over_2 = 1;
+
+        while(over_1 == 0 || over_2 == 0);
+
+        for(int i = 512; i < 1024; i++)
+            pmem_free((uint64)mem[i], true);
+        printf("cpu %d free over\n", cpuid);        
+ 
+    }
     while (1);    
-}  
+}
+// #include "riscv.h"
+// #include "lib/print.h"
+
+// volatile static int started = 0;
+
+// int main()
+// {
+//     int cpuid = r_tp();
+//     if(cpuid == 0) {
+//         print_init();
+//         printf("cpu %d is booting!\n", cpuid);        
+//         __sync_synchronize();
+//         started = 1;
+//     } else {
+//         while(started == 0);
+//         __sync_synchronize();
+//         printf("cpu %d is booting!\n", cpuid);
+//     }   
+//     while (1);    
+// }  
 
 // #include "riscv.h"
 // #include "lib/print.h"
