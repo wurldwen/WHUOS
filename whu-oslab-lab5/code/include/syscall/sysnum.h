b492f1397864e5d2e53d0546fa62b577115805a8
wurld <1287356875@qq.com>
2025-11-03 16:42:50 +0800

    添加syscall相关文件

---
+++ whu-oslab-lab5/code/include/syscall/sysnum.h
@@ -0,0 +1,11 @@
+#ifndef __SYSNUM_H__
+#define __SYSNUM_H__
+
+#define SYS_brk          1
+#define SYS_mmap         2
+#define SYS_munmap       3
+#define SYS_copyin       4
+#define SYS_copyout      5
+#define SYS_copyinstr    6
+
+#endif
