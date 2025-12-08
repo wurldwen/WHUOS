5b6ed7bc5c462b0ccca7678a2735a31e1eb1a213
wurld <1287356875@qq.com>
2025-12-08 16:47:53 +0800

    解决初始化问题

--- whu-oslab-lab7/code/kernel/fs/fs.c
+++ whu-oslab-lab7/code/kernel/fs/fs.c
@@ -13,8 +13,8 @@ super_block_t sb;
 #define SB_BLOCK_NUM 0
 
 // 测试用的数组（暂未使用）
-// static uint8 str[BLOCK_SIZE * 2];
-// static uint8 tmp[BLOCK_SIZE * 2];
+static uint8 str[BLOCK_SIZE * 2];
+static uint8 tmp[BLOCK_SIZE * 2];
 
 // 比较两个大小为 2*BLOCK_SIZE 的空间是否完全一样（暂未使用）
 
@@ -62,38 +62,38 @@ void fs_init()
     printf("\nFile system initialized successfully!\n");
     uint32 ret = 0;
 
-    for(int i = 0; i < BLOCK_SIZE * 2; i++)
-        str[i] = i;
+//     for(int i = 0; i < BLOCK_SIZE * 2; i++)
+//         str[i] = i;
 
-    // 创建新的inode
-    inode_t* nip = inode_create(FT_FILE, 0, 0);
-    inode_lock(nip);
-    
-    // 第一次查看
-    inode_print(nip);
+//     // 创建新的inode
+//     inode_t* nip = inode_create(FT_FILE, 0, 0);
+//     inode_lock(nip);
+//     
+//     // 第一次查看
+//     inode_print(nip);
 
-    // 第一次写入
-    ret = inode_write_data(nip, 0, BLOCK_SIZE / 2, str, false);
-    assert(ret == BLOCK_SIZE / 2, "inode_write_data: fail");
+//     // 第一次写入
+//     ret = inode_write_data(nip, 0, BLOCK_SIZE / 2, str, false);
+//     assert(ret == BLOCK_SIZE / 2, "inode_write_data: fail");
 
-    // 第二次写入
-    ret = inode_write_data(nip, BLOCK_SIZE / 2, BLOCK_SIZE + BLOCK_SIZE / 2, str + BLOCK_SIZE / 2, false);
-    assert(ret == BLOCK_SIZE +  BLOCK_SIZE / 2, "inode_write_data: fail");
+//     // 第二次写入
+//     ret = inode_write_data(nip, BLOCK_SIZE / 2, BLOCK_SIZE + BLOCK_SIZE / 2, str + BLOCK_SIZE / 2, false);
+//     assert(ret == BLOCK_SIZE +  BLOCK_SIZE / 2, "inode_write_data: fail");
 
-    // 一次读取
-    ret = inode_read_data(nip, 0, BLOCK_SIZE * 2, tmp, false);
-    assert(ret == BLOCK_SIZE * 2, "inode_read_data: fail");
+//     // 一次读取
+//     ret = inode_read_data(nip, 0, BLOCK_SIZE * 2, tmp, false);
+//     assert(ret == BLOCK_SIZE * 2, "inode_read_data: fail");
 
-    // 第二次查看
-    inode_print(nip);
-    
-    inode_unlock_free(nip);
+//     // 第二次查看
+//     inode_print(nip);
+//     
+//     inode_unlock_free(nip);
 
-    // 测试
-    if(blockcmp(tmp, str) == true)
-        printf("success");
-    else
-        printf("fail");
+//     // 测试
+//     if(blockcmp(tmp, str) == true)
+//         printf("success");
+//     else
+//         printf("fail");
 
-    while (1); 
+//     while (1); 
 }
